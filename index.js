d5c141bab42932eb7a0b8a53c1311df1afdabe1c
tommygaessler <tommy.gaessler@gmail.com>
2020-03-02 16:06:10 -0700

    removed request package not being used

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
-const request = require('request')
 const crypto = require('crypto')
 const cors = require('cors')
 
