7908e9da02cea12a969c792686565f746882f462
michaelZoomDA <39344486+michaelZoomDA@users.noreply.github.com>
2020-03-29 21:44:26 -0700

    modify timestamp for generating signature

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.options('*', cors());
 
 app.post('/', (req, res) => {
 
-  const timestamp = new Date().getTime()
+  const timestamp = new Date().getTime() - 30000
   const msg = Buffer.from(process.env.API_KEY + req.body.meetingNumber + timestamp + req.body.role).toString('base64')
   const hash = crypto.createHmac('sha256', process.env.API_SECRET).update(msg).digest('base64')
   const signature = Buffer.from(`${process.env.API_KEY}.${req.body.meetingNumber}.${timestamp}.${req.body.role}.${hash}`).toString('base64')
